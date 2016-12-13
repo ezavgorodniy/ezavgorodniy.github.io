1be81c13122f1a4a165fc49e8b86a70e6fe9bb23
ezavgorodniy <eugurzav@mail.ru>
2016-12-13 15:43:32 +0000

    Add test page

---
+++ src/scripts/TestJavascript.js
@@ -0,0 +1,10 @@
+/**
+ * Created by yevhen.zavhorodnii on 13/12/2016.
+ */
+
+$(document).ready(function () {
+        $("#testLink").click(function () {
+            alert("Test link have been clicked");
+        });
+    }
+);
